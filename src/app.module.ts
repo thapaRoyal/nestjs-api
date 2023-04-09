78d715bdbb9e30694e0383be05fa9ef5740d6d28
thapaRoyal <thaparoyal17@gmail.com>
2023-04-09 20:48:03 +0545

    Modules and dependency injection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
 
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, BookmarkModule],
   controllers: [],
   providers: [],
 })
