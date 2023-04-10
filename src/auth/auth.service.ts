ea7623abb3ec3d033cfd850b3019a7c077e173ce
thapaRoyal <thaparoyal17@gmail.com>
2023-04-10 20:44:26 +0545

    Signup

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,17 +1,42 @@
-import { Injectable } from '@nestjs/common';
-import {User, Bookmark} from '@prisma/client'
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
+import * as argon from 'argon2';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma:PrismaService){}
-  signup(dto: AuthDto) {
-    
-    return 'I am signup';
+  constructor(private prisma: PrismaService) {}
+
+  async signup(dto: AuthDto): Promise<User> {
+    // generate the password hash
+    const hash = await argon.hash(dto.password);
+
+    try {
+      // save the new user in db
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+
+      // remove the hash from the user object before returning it
+      delete user.hash;
+
+      return user;
+    } catch (error) {
+      // if the email is already taken, throw a BadRequestException with the error message
+      if (error.code === 'P2002' && error.meta?.target?.includes('email')) {
+        throw new BadRequestException('Email already taken');
+      }
+
+      // re-throw the error if it's not related to the email being already taken
+      throw error;
+    }
   }
 
-  signin(){
+  signin() {
     return 'I am signin';
   }
 }
