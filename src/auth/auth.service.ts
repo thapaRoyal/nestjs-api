9d5bb46f99405f937f46865e9facc2f46eb087ad
thapaRoyal <thaparoyal17@gmail.com>
2023-04-10 08:51:43 +0545

    nestjs globalpipes

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import {User, Bookmark} from '@prisma/client'
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma:PrismaService){}
-  signup() {
+  signup(dto: AuthDto) {
+    
     return 'I am signup';
   }
 
